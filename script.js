9cdba9e18f0736b59646984466aa0040b7cf6237
Victoria Shevchenko <shevviktoryia@gmail.com>
2024-11-23 21:09:35 +0300

    feat: add field and snake

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let blockSize = 25;
 let rows = 20;
 let cols = 20;
-let board, context;
+let field, context;
+
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+window.onload = () => {
+  field = document.querySelector("#game-field");
+  field.height = rows * blockSize;
+  field.width = cols * blockSize;
+  context = field.getContext("2d");
+  update();
+};
+
+function update() {
+  context.fillStyle = "darkgreen";
+  context.fillRect(0, 0, field.width, field.height);
+
+  context.fillStyle = "yellow";
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+}
