2bf9989dd4a3694015a0c1246ae5e368d53dc9e0
Victoria Shevchenko <shevviktoryia@gmail.com>
2024-11-30 20:39:01 +0300

    feat: add food generation

--- script.js
+++ script.js
@@ -6,18 +6,29 @@ let field, context;
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
 
+let foodX, foodY;
+
 window.onload = () => {
   field = document.querySelector("#game-field");
   field.height = rows * blockSize;
   field.width = cols * blockSize;
   context = field.getContext("2d");
-  update();
+  generateFood();
+  updateField();
 };
 
-function update() {
+function updateField() {
   context.fillStyle = "darkgreen";
   context.fillRect(0, 0, field.width, field.height);
 
   context.fillStyle = "yellow";
   context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  context.fillStyle = "red";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function generateFood() {
+  foodX = Math.floor(Math.random() * cols) * blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
 }
