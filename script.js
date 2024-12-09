41942f7a3344f16b5a323172a51ed2978d444af8
Victoria Shevchenko <shevviktoryia@gmail.com>
2024-12-09 23:06:51 +0300

    feat: add end

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let field, context;
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
 
+const snake = [];
+
+let gameOver = false;
+
 let foodX,
   foodY,
   velX = 0,
@@ -22,8 +26,17 @@ window.onload = () => {
 };
 
 function updateField() {
+  if (gameOver) return;
+
   context.fillStyle = "darkgreen";
   context.fillRect(0, 0, field.width, field.height);
+
+  for (let i = snake.length - 1; i > 0; i--) {
+    snake[i] = snake[i - 1];
+  }
+  if (snake.length) {
+    snake[0] = [snakeX, snakeY];
+  }
 
   context.fillStyle = "yellow";
   snakeX += velX * blockSize;
@@ -34,7 +47,29 @@ function updateField() {
   context.fillRect(foodX, foodY, blockSize, blockSize);
 
   if (snakeX === foodX && snakeY === foodY) {
+    snake.push([foodX, foodY]);
     generateFood();
+  }
+
+  for (let i = 0; i < snake.length; i++) {
+    context.fillRect(snake[i][0], snake[i][1], blockSize, blockSize);
+  }
+
+  if (
+    snakeX < 0 ||
+    snakeX > cols * blockSize ||
+    snakeY < 0 ||
+    snakeY > rows * blockSize
+  ) {
+    gameOver = true;
+    alert("game over");
+  }
+
+  for (let i = 0; i < snake.length; i++) {
+    if (snakeX == snake[i][0] && snakeY == snake[i][1]) {
+      gameOver = true;
+      alert("game over");
+    }
   }
 }
 
